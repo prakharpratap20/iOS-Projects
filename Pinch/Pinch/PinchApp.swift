cbd33217f207de3f13ace03c2b89208225179e49
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-09-06 23:55:33 +0530

    sf files

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Prakhar Pratap on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
